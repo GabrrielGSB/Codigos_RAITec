6603e2162dfddccf413d3f7a5419ab0a6a73766c
GabrrielGSB <g.gonzaga.barreto@gmail.com>
2024-07-18 16:02:24 -0300

    Add files via upload

--- Receptor.ino
+++ Receptor.ino
@@ -8,113 +8,61 @@ int valor_recebido_2;
 int valor_recebido_3;
 int valor_recebido_4;
 int valor_recebido_5;
-//char received;
-void setup() {
-  
-Serial.begin(115200);
-
-RxSerial.begin(4800,SERIAL_8N1,16,17);
 
 
+void setup() 
+{  
+  Serial.begin(115200);
+  RxSerial.begin(4800,SERIAL_8N1,16,17);
 }
 
-void loop() {
-
-String received = "";
-
-
-    while (RxSerial.available()){
+void loop() 
+{
+  String received = "";
+  while (RxSerial.available())
+  {
     received = char(RxSerial.read());
-    //received = RxSerial.read();
     Serial.println(received);
     
-    //valor_recebido=atoi(received);
-    if(received == "A"){
-        Serial.println("Dados de vibração sendo recebidos:");
-        //while (RxSerial.available()){
+    if(received == "A")
+    {
+      received = char(RxSerial.read());
+      valor_recebido_1 = atoi(received.c_str());   
+      while(valor_recebido_1 == 0 )
+      {
         received = char(RxSerial.read());
-        valor_recebido_1 = atoi(received.c_str());   
-            while(valor_recebido_1 == 0 ){
-            received = char(RxSerial.read());
-            delay(1);  
-            valor_recebido_1 = atoi(received.c_str());                 
-            }
-        delay(1);
-        
-        received = char(RxSerial.read());
-        valor_recebido_2 = atoi(received.c_str());   
-        delay(1);
+        delay(1);  
+        valor_recebido_1 = atoi(received.c_str());                 
+      }
+      delay(1);
+      
+      received = char(RxSerial.read());
+      valor_recebido_2 = atoi(received.c_str());   
+      delay(1);
 
-        received = char(RxSerial.read());
-        valor_recebido_3 = atoi(received.c_str());   
-        delay(1);
+      received = char(RxSerial.read());
+      valor_recebido_3 = atoi(received.c_str());   
+      delay(1);
 
-        received = char(RxSerial.read());
-        valor_recebido_4 = atoi(received.c_str());   
-        delay(1);
+      received = char(RxSerial.read());
+      valor_recebido_4 = atoi(received.c_str());   
+      delay(1);
 
-        received = char(RxSerial.read());
-        valor_recebido_5 = atoi(received.c_str());   
-        delay(1);
-        
-        valor_recebido_1 = valor_recebido_1*1000;
-        valor_recebido_2 = valor_recebido_2*100;
-        valor_recebido_3 = valor_recebido_3*10;
-        valor_recebido = valor_recebido_4 + valor_recebido_3 + valor_recebido_2 + valor_recebido_1;
-        Serial.print("Valor recebido de vibração:");
-        Serial.println(valor_recebido);
-
-
-
-        Serial.println(valor_recebido);
-        Serial.print(valor_recebido_1);
-        Serial.print(valor_recebido_2);
-        Serial.print(valor_recebido_3);
-        Serial.print(valor_recebido_4);
-        Serial.println(valor_recebido_5);
-        //}
-
-    }
-
-    if(received == "B"){
-        Serial.println("Dados de luminosidade sendo recebidos:");
-       // while (RxSerial.available()){
-        valor_recebido_1 = atoi(received.c_str());   
-            while(valor_recebido_1 == 0 ){
-            received = char(RxSerial.read());
-            delay(1);
-            valor_recebido_1 = atoi(received.c_str());     
-            }
-        delay(1);
-
-        received = char(RxSerial.read());
-        valor_recebido_2 = atoi(received.c_str());   
-        delay(1);
-
-        received = char(RxSerial.read());
-        valor_recebido_3 = atoi(received.c_str());   
-        delay(1);
-
-        received = char(RxSerial.read());
-        valor_recebido_4 = atoi(received.c_str());   
-        delay(1);
-
-        received = char(RxSerial.read());
-        valor_recebido_5 = atoi(received.c_str());   
-        delay(1);
-        valor_recebido_1 = valor_recebido_1*1000;
-        valor_recebido_2 = valor_recebido_2*100;
-        valor_recebido_3 = valor_recebido_3*10;
-        valor_recebido = valor_recebido_4 + valor_recebido_3 + valor_recebido_2 + valor_recebido_1;
-        Serial.print("Valor recebido de luminosidade:");
-        Serial.println(valor_recebido);
-        Serial.print(valor_recebido_1);
-        Serial.print(valor_recebido_2);
-        Serial.print(valor_recebido_3);
-        Serial.print(valor_recebido_4);
-        Serial.println(valor_recebido_5);
-       // }
-
+      received = char(RxSerial.read());
+      valor_recebido_5 = atoi(received.c_str());   
+      delay(1);
+      
+//      valor_recebido_1 = valor_recebido_1*1000;
+//      valor_recebido_2 = valor_recebido_2*100;
+//      valor_recebido_3 = valor_recebido_3*10;
+//      valor_recebido = valor_recebido_4 + valor_recebido_3 + valor_recebido_2 + valor_recebido_1;
+//      Serial.print("Valor recebido de vibração:");
+//      Serial.println(valor_recebido);
+      Serial.print(valor_recebido_1);
+      Serial.print(valor_recebido_2);
+      Serial.print(valor_recebido_3);
+      Serial.print(valor_recebido_4);
+      Serial.println(valor_recebido_5);
     }
   }
 }
--- Transmissor.ino
+++ Transmissor.ino
@@ -8,100 +8,36 @@
  */
 //Inclusão de Bibliotecas
 
-
 #include <SoftwareSerial.h>
-//#include "LowPower.h"
+
 
 SoftwareSerial RF(8,9);
 
-//Sensor de Luminosidade
-int valor_luminosidade; 
-int pino_ldr = A0;
-
-//Sensor de Vibração
-int valor_vibracao; 
-int pino_piezo = A2;
-
-int led_inicio = 13;
+String codigo;
 
 void setup() {
   
 Serial.begin(9600);
-
 RF.begin(4800);
-
-Serial.println("Teste:");
-
 pinMode(led_inicio, OUTPUT);
-
-
 }
 
-void loop() {
-
-//Ler luminosidade
-valor_luminosidade = analogRead(pino_ldr);
-Serial.print("Luminosidade:");
-Serial.println(valor_luminosidade);
-delay(500);
-
-//Ler vibração
-valor_vibracao = analogRead(pino_piezo);
-valor_vibracao = valor_vibracao;
-Serial.print("Vibração:");
-Serial.println(valor_vibracao);
-delay(500);
-
-
-
-//Enviar luminosidade para a central poderosa
-String codigo = "B";
-enviar_dado(valor_luminosidade,codigo);
-
-delay(5000);
-
-//Enviar vibracao para a central poderosa
-codigo = "A";
-enviar_dado(valor_vibracao, codigo);
-
-delay(5000);
- 
-//dormir();
-
-
-}
-
-void enviar_dado(int medicao, String codigo){
-
-for(int i = 0; i < 5 ; i++){
-RF.flush();
-RF.println(codigo);
-RF.println(medicao);
-
-}
-
-digitalWrite(led_inicio, HIGH);
-delay(500);
-digitalWrite(led_inicio, LOW);
-
-delay(5000);
-
+void loop() 
+{
+  //Enviar luminosidade para a central poderosa
+  codigo = "B";
+  enviar_dado(valor_luminosidade,codigo);
   
+  delay(5000);
 }
 
-//void dormir () {
-//  //Modo Economia Atmega e módulos
-//Serial.println("Dormindo");
-//
-//// ROTINA PARA COLOCAR O MICROCONTROLADOR ATMEGA328 PARA DORMIR
-//for (int i=0; i<10; i++){
-//
-//LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);  
-//
-//}
-//
-//// ROTINA PARA COLOCAR O DHT11 PARA DORMIR
-//
-//
-//Serial.println("Acordei!");
-//}
+void enviar_dado(int medicao, String codigo)
+{
+  for(int i = 0; i < 5 ; i++)
+  {
+    RF.flush();
+    RF.println(codigo);
+    RF.println(medicao);
+  }
+  delay(5000);
+}
