4aac1f2e0ef7e8d27b8bcfc361d5d7ebdb329234
GabrrielGSB <g.gonzaga.barreto@gmail.com>
2024-07-24 02:05:16 -0300

    Update ESP32_Servidor.ino

--- ESP32_Servidor.ino
+++ ESP32_Servidor.ino
@@ -1,14 +1,23 @@
 #include <WiFi.h>
+#include <stdlib.h>  // Necessário para usar a função atof()
+
 // Porta do servidor
 WiFiServer server(80);
 
 void LoopCore0(void *pvParameters);
 
-String mensagem;
+float PID[9]     = {2,0,0, 0.6,3.5,0.03, 1,6,0},
+      PIDcopy[9] = {2,0,0, 0.6,3.5,0.03, 1,6,0};
+size_t pidSize = sizeof(PID)/sizeof(float);
+
+int    adress, indexInDrone;
+float  mod,    modInDrone;
+String cmd,    data,       valorPID = "";
 
 // Configurações do ponto de acesso (AP)
-const char *ssid     = "ESP32_Server";
-const char *password = "123456789";
+const char *ssid     = "ESP32_Server",
+           *password = "123456789";
+
 
 void setup() 
 {
@@ -39,13 +48,38 @@ void LoopCore0(void *pvParameters)
 {
   while(1)
   {
-    if (Serial.available() > 0)
+    if(Serial.available())
     {
-      controlString = Serial.readStringUntil('\n');
-      Serial.println(controlString);
+      cmd = Serial.readString();
+      if (cmd == "reset\n") copiarLista(PIDcopy, PID, 9);
+        
+      adress = (cmd[0] - '0') -1;
+      mod = atof(&(cmd[2]));
+
+      Serial.printf("\nO comando chamado é: %s\n",cmd);
+
+      PID[adress] += mod;
+      for (int i = 0 ; i < pidSize ; i++)
+      {
+        Serial.printf("%0.2f \n", PID[i]);
+      }
     }
-    
-    vTaskDelay(250 / portTICK_PERIOD_MS);
+    enviarDados();
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // if (Serial.available() > 0)
+    // {
+    //   controlString = Serial.readStringUntil('\n').c_str();
+
+    //   p[0] = controlString[0];
+    //   p[1] = controlString[1];
+
+    //   String parametro(p);
+
+    //   ajuste = atof(controlString+3);
+
+    //   Serial.printf("O parâmetro a ser ajustado: %c/n", parametro);
+    //   Serial.printf("O ajuste feito é: %f", ajuste);
+    // }
   }
 }
 
@@ -69,12 +103,19 @@ void loop()
     client.stop();
     Serial.println("Client Disconnected");
   }
-  // else 
-  // {
-  //   while (client.connected() == false)
-  //   {
-  //     Serial.println("Esperando o cliente...");
-  //     delay(10000);
-  //   }
-  // }
 }
+
+void copiarLista(float* fonte, float* copia, int tamanhoLista) 
+{
+  for (int i = 0; i < tamanhoLista; i++) copia[i] = fonte[i];
+}
+
+void enviarDados()
+{
+  valorPID = "";
+  for (int i = 0; i < 9; i++) valorPID += (String(PID[i]) + String(" "));  
+
+  Serial.println(valorPID);
+}
+
+
