d5ed6f043b3cc75f13446a35f8ae930542eeddb2
GabrrielGSB <g.gonzaga.barreto@gmail.com>
2024-07-24 21:16:20 -0300

    Update ESP32_Cliente.ino

--- ESP32_Cliente.ino
+++ ESP32_Cliente.ino
@@ -1,5 +1,13 @@
 #include <WiFi.h>
 
+float PID[9]     = {2,0,0, 0.6,3.5,0.03, 1,6,0},
+      PIDcopy[9] = {2,0,0, 0.6,3.5,0.03, 1,6,0},
+      mod;
+
+size_t pidSize = sizeof(PID)/sizeof(float);
+
+int adress;
+
 // Configurações da rede WiFi do servidor (AP)
 const char *ssid = "ESP32_Server";
 const char *password = "123456789";
@@ -8,12 +16,13 @@ const char *password = "123456789";
 const char *host = "192.168.4.1"; // IP do servidor ESP32
 const uint16_t port = 80;
 
+String cmd;
+
 WiFiClient client;
 
 void setup() {
   Serial.begin(115200);
-
-  delay(3000);
+  while(!Serial){}
   // Conecta-se à rede WiFi do servidor
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi");
@@ -23,48 +32,42 @@ void setup() {
     Serial.print(".");
   }
   Serial.println(" Connected");
-
-  // Conecta-se ao servidor
-  if (!client.connect(host, port)) {
-    Serial.println("Connection to server failed");
-    return;
-  }
-
-  // Envia uma mensagem para o servidor
-  client.println("Primeira conexão com o servidor");
-
-  // Recebe a resposta do servidor
-  while (client.connected()) {
-    if (client.available()) {
-      String response = client.readStringUntil('\n');
-      Serial.println("Server response: " + response);
-      break;
-    }
-  }
-
-  client.stop();
 }
 
 void loop() 
 {
-  delay(5000);
-  if (!client.connect(host, port)) {
+  delay(500);
+  cmd = "";
+  // Serial.println();
+  if (!client.connect(host, port)) 
+  {
     Serial.println("Connection to server failed");
     return;
   }
 
-  // Envia uma mensagem para o servidor
-  client.println("Conexões REPETIDAS COM O SERVIDOR");
-
-  // Recebe a resposta do servidor
-  while (client.connected()) {
-    if (client.available()) {
-      String response = client.readStringUntil('\n');
-      Serial.println("Server response: " + response);
+  while (client.connected()) 
+  {
+    if (client.available()) 
+    {
+      cmd = client.readStringUntil('\n');
+      Serial.println("Mensagem do Servidor: " + cmd);
       break;
     }
   }
 
   client.stop();
+
+  adress = (cmd[0] - '0') -1;
+  mod = atof(&(cmd[2]));
+
+  PID[adress] += mod;
+  if (cmd == "r") copiarLista(PIDcopy, PID, 9);
+  for (int i = 0 ; i < pidSize ; i++) Serial.printf("%0.2f \n", PID[i]);
 }
+
+void copiarLista(float* fonte, float* copia, int tamanhoLista) 
+{
+  for (int i = 0; i < tamanhoLista; i++) copia[i] = fonte[i];
+}
+
 
