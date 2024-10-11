dd2aa00b2d46dbe22083b6b950819923d9f03dda
GabrrielGSB <g.gonzaga.barreto@gmail.com>
2024-10-11 15:25:39 -0300

    Update trab_tecnico_3.ino

--- trab_tecnico_3.ino
+++ trab_tecnico_3.ino
@@ -1,56 +1,90 @@
+#include <ESP32Servo.h>
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define SS_PIN   21  // SDA
-#define RST_PIN  22 // RST
+#define SS_PIN 21   // SDA
+#define RST_PIN 22  // RST
 #define comando1 27
-#define comando2 26
+#define porta    26  // porta
 #define comando3 25
-#define comando4 33
+#define motor    33
 
-bool corretaTAG = 0;
+bool corretaTAG = 0,
+     estadoPorta = 0,
+     estadoPortaOld = 0;
 
 unsigned long tempoAntigo = 0;
 unsigned long tempoAtual;
-uint16_t  intervalo = 2000;
+uint16_t intervalo = 2000;
+uint8_t  contPorta = 0;
 
-MFRC522 mfrc522(SS_PIN, RST_PIN); // Cria um objeto MFRC522
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Cria um objeto MFRC522
+Servo motorPorta;
 
 void setup() {
-  Serial.begin(115200); // Inicializa a comunicação serial
+  Serial.begin(115200);  // Inicializa a comunicação serial
   delay(2000);
-  SPI.begin();          // Inicializa o barramento SPI
-  mfrc522.PCD_Init();   // Inicializa o MFRC522
+  SPI.begin();         // Inicializa o barramento SPI
+  mfrc522.PCD_Init();  // Inicializa o MFRC522
 
   pinMode(comando1, OUTPUT);
-  pinMode(comando2, OUTPUT);
+  pinMode(porta, INPUT);
   pinMode(comando3, OUTPUT);
-  pinMode(comando4, OUTPUT);
+  pinMode(motor, OUTPUT);
 
   digitalWrite(comando1, LOW);
-  digitalWrite(comando2, LOW);
   digitalWrite(comando3, LOW);
-  digitalWrite(comando4, LOW);
+ 
+  estadoPortaOld = 1;
 }
 
-void loop() 
-{
-   tempoAtual = millis();
-  if (tempoAtual - tempoAntigo >= intervalo) // Taxa de atualização do loop de controle
+void loop() {
+  tempoAtual = millis();
+  if (tempoAtual - tempoAntigo >= intervalo)  // Taxa de atualização do loop de controle
   {
     tempoAntigo = tempoAtual;
-    if (verificaTAG()) 
-    {
+    if (verificaTAG()) {
       digitalWrite(comando1, HIGH);
       delay(5000);
+    } else digitalWrite(comando1, LOW);
+  }
+  controlePorta();
+
+//  Serial.println(porta.read());
+}
+
+void controlePorta() 
+{
+//  estadoPorta = !estadoPorta;
+  estadoPorta = digitalRead(porta);
+  
+  if (estadoPorta != estadoPortaOld) 
+  {
+    
+    estadoPortaOld = estadoPorta;
+    if ( (estadoPorta == 0) && (contPorta >= 1) ) 
+    {
+      motorPorta.attach(33);
+      motorPorta.write(1);
+      delay(750);
+      motorPorta.detach();
+      delay(2000);
     }
-    else               digitalWrite(comando1, LOW);
+
+    if ( (estadoPorta == 1) && (contPorta >= 1) ) 
+    {
+      motorPorta.attach(33);
+      motorPorta.write(200);
+      delay(750);
+      motorPorta.detach();
+      delay(2000);
+    }
+    contPorta++;
   }
 }
 
-bool verificaTAG()
-{
- // Verifica se há uma nova tag presente
+bool verificaTAG() {
+  // Verifica se há uma nova tag presente
   corretaTAG = true;
   if (!mfrc522.PICC_IsNewCardPresent()) {
     return !corretaTAG;
@@ -63,8 +97,7 @@ bool verificaTAG()
 
   // Imprime o UID da tag
   Serial.print("UID da tag: ");
-  for (byte i = 0; i < mfrc522.uid.size; i++) 
-  {
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
     Serial.print(mfrc522.uid.uidByte[i], HEX);
   }
@@ -73,3 +106,24 @@ bool verificaTAG()
 
   return corretaTAG;
 }
+
+
+//  fechada = 50;
+//  aberto = 100;
+//
+//  if (estadoPorta != estadoPortaOld) {
+//    if (estadoPorta == 0) {
+//      for (int i = fechada; i == aberto; i++) {  // abre
+//        porta.write(i);
+//        delay(100);
+//      }
+//      estadoPortaOld = estadoPorta;
+//    }
+//    if (estadoPorta == 1) {
+//      for (int j = aberto; j == fechada; j--) {  // fecha
+//        porta.write(j);
+//        delay(100);
+//      }
+//      estadoPortaOld = estadoPorta;
+//    }
+//  }
