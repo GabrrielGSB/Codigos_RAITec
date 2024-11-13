c79e560b6b087b8d3e53f3b2271aadd99159e903
GabrrielGSB <g.gonzaga.barreto@gmail.com>
2024-11-13 12:48:10 -0300

    Update Dtec.ino

--- Dtec.ino
+++ Dtec.ino
@@ -1,13 +1,8 @@
-// if (tempoAtual - tempoAntigo >= intervalo) 
-// {
+#include "driver/ledc.h"
 
-// }
-
-#include "esp_task_wdt.h"  // Biblioteca para manipular o WDT
-
-
-#define SensorPartida 15
-#define SensorFim     2
+#define SensorPartida 33
+#define SensorFim     25
+#define buzzer        27
 
 String nomeEquipe, 
        tempoTentativa,
@@ -28,25 +23,31 @@ unsigned long tempoAntigo = 0,
               millisecs,
               inicioContagem,
               tempoFinal,
-              tempoDecorrido;         
+              tempoDecorrido;
+
+const int pwmPin = 27;   // Pino para o sinal PWM
+const int pwmChannel = 0; // Canal PWM (de 0 a 15)
+const int pwmFreq = 5000; // Frequência em Hz
+const int pwmResolution = 8; // Resolução do duty cycle (8 bits, de 0 a 255)
 
 void setup() 
 {
+  ledcSetup(pwmChannel, pwmFreq, pwmResolution);
+  ledcAttachPin(pwmPin, pwmChannel);
+  
   Serial.begin(115200);
   pinMode(SensorPartida, INPUT);
   pinMode(SensorFim,     INPUT);
+  pinMode(buzzer,        OUTPUT);
+  
+  digitalWrite(buzzer, LOW);
 }
 
 void loop() 
 {
-//    int a = analogRead(SensorPartida);
-//    int b = analogRead(SensorFim);
-//    Serial.println(a);
-//    Serial.println(b);
-//    Serial.println("");
-//    delay(100);
   if (Serial.available())
   {
+    tocarMusica(1000, 500);
     while (carrinhoCompetindo == false)
     {
       if (equipeCadastrada == false) cadastrarEquipe();
@@ -92,17 +93,16 @@ void esperarPartida()
   carrinhoCompetindo = true;
 }
 
-//void comandarPartida()
-//{
-//  if (Serial.available()) comando = Serial.readStringUntil(' ');
-//  
-//  if (comando == "pronto\n") carrinhoCompetindo = true;
-//  else                       carrinhoCompetindo = false;
-//}
-
 void esperarTermino()
 {
   Serial.println("Carrinho Começou a Competição!!!");
+
+  for (int i = 0; i > 50; i++);
+  {
+    tocarMusica(1000, 50);
+    delay(100);
+  }
+  
   while(carrinhoCompetindo == true) 
   {
     receberSensor();
@@ -115,6 +115,10 @@ void esperarTermino()
 void obterTempo()
 {
   Serial.println("Carrinho completou o percurso!!!");
+  tocarMusica(500, 5000);
+  delay(100);
+  tocarMusica(500, 5000);
+  delay(100);
   tempoFinal     = millis();
   tempoDecorrido = tempoFinal - inicioContagem;
   tempoTentativa = "Tempo:";
@@ -127,12 +131,12 @@ void obterTempo()
   if (minutes < 10) tempoTentativa += "0" + String(minutes);  
   else              tempoTentativa += String(minutes);
   
-  tempoTentativa += ";";
+  tempoTentativa += ":";
 
   if (seconds < 10) tempoTentativa += "0" + String(seconds);  
   else              tempoTentativa += String(seconds);
 
-  tempoTentativa += ";";
+  tempoTentativa += ":";
 
   if      (millisecs < 10)  tempoTentativa += "00" + String(millisecs);  
   else if (millisecs < 100) tempoTentativa += "0" + String(millisecs);   
@@ -140,3 +144,24 @@ void obterTempo()
  
   Serial.println(tempoTentativa);
 }
+
+void tocarMusica(uint16_t intervalo, uint16_t freq)
+{
+  tone(buzzer, freq);
+  delay(intervalo);
+  noTone(buzzer);
+}
+//void comandarPartida()
+//{
+//  if (Serial.available()) comando = Serial.readStringUntil(' ');
+//  
+//  if (comando == "pronto\n") carrinhoCompetindo = true;
+//  else                       carrinhoCompetindo = false;
+//}
+
+//    int a = analogRead(SensorPartida);
+//    int b = analogRead(SensorFim);
+//    Serial.println(a);
+//    Serial.println(b);
+//    Serial.println("");
+//    delay(100);
